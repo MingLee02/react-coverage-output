75390f355c4dc5082f6175739d8480912e1053f9
MingLee02 <ming.lee@hotmail.com>
2017-07-16 17:56:49 +0100

    Display total coverage.

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -3,6 +3,12 @@ import json from '../data/test.json';
 import Table from './table';
 
 
+const totalCoverage = {
+    'statements': 0,
+    'missing': 0,
+    'coverage': 0
+}
+
 export const getCoverage = function(statements=0, tests=0) {
     if (!isNaN(statements) && !isNaN(tests)) {
         return 100 - (tests / statements * 100).toFixed(2) + '%';
@@ -28,7 +34,10 @@ export const buildTableData = function(key) {
     const missing = value[1] - value[0]
     const coverage = getCoverage(value[1], missing)
     const path = getPath(key)
-        
+
+    totalCoverage.statements += value[1]
+    totalCoverage.missing += value[0]
+    
     return {
         'path': path,
         'name': key,
@@ -39,17 +48,24 @@ export const buildTableData = function(key) {
     }
 }
 
-
 export default React.createClass({
   render: function() {
     const itemArr = []
 
     for(let key in json) {
-        itemArr.push(buildTableData(key))
+        itemArr.push(buildTableData(key));
+        totalCoverage.coverage = getCoverage(totalCoverage.statements, totalCoverage.missing);
     }
 
     return (
         <div>
+            <h1>Coverage Report</h1>
+            <div>
+                <h2>Total</h2> 
+                <p><strong>Lines of Code:</strong> {totalCoverage.statements}</p>
+                <p><strong>Missing:</strong> {totalCoverage.missing}</p>
+                <p><strong>Coverage:</strong> {totalCoverage.coverage}</p>
+            </div>
             <Table data={itemArr} />
         </div>
     );
